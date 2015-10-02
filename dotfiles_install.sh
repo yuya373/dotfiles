b8d9f265b68d177eb2e391ef2a2dcbb256ce0ae3
Yuya Minami <yuya.minami@aktsk.jp>
2015-10-02 11:26:16 +0900

    update shellscript

--- dotfiles_install.sh
+++ dotfiles_install.sh
@@ -1,3 +1,5 @@
+set -e
+
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.vimshrc ~/.vimshrc
 ln -sf ~/dotfiles/.gvimrc ~/.gvimrc
@@ -13,5 +15,8 @@ ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/.tmux-powerlinerc.default ~/.tmux-powerlinerc
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
 mkdir -p ~/.config/common-lisp/
+mkdir -p ~/dev/cl
 ln -sf ~/dotfiles/.config/common-lisp/source-registry.conf ~/.config/common-lisp/source-registry.conf
 ln -sf ~/dotfiles/.sbclrc ~/.sbclrc
+mkdir ~/.emacs.d/
+ln -sf ~/dotfiles/.emacs.d/init.el ~/.emacs.d/init.el
