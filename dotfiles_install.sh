eb3d3f0fc23ecd3bb787a72ae1a0edf7e1f9df28
Yuya Minami <yuya373@me.com>
2015-11-17 21:58:09 +0900

    update dotfiles_install

--- dotfiles_install.sh
+++ dotfiles_install.sh
@@ -1,22 +1,53 @@
 set -e
 
+# git
+ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
+
+# vim
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.vimshrc ~/.vimshrc
 ln -sf ~/dotfiles/.gvimrc ~/.gvimrc
 ln -sf ~/dotfiles/.xvimrc ~/.xvimrc
+
+# zsh
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/.zshenv ~/.zshenv
+
+# prezto
+if [[ ! -d ~/.zprezto ]]; then
+    ln -sf ~/dotfiles/.zprezto ~/.zprezto
+fi
 ln -sf ~/dotfiles/.zprezto/runcoms/zlogin ~/.zlogin
 ln -sf ~/dotfiles/.zprezto/runcoms/zlogout ~/.zlogout
-ln -sf ~/dotfiles/.zprezto ~/.zprezto
 ln -sf ~/dotfiles/.zpreztorc ~/.zpreztorc
 ln -sf ~/dotfiles/.zprofile ~/.zprofile
+
+# tmux
+
+if [[ ! -d ~/.tmux/plugins/tpm ]]; then
+    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+fi
+if [[ ! -d ~/tmux-powerline ]]; then
+    git clone https://github.com/erikw/tmux-powerline.git ~/tmux-powerline
+fi
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/.tmux-powerlinerc.default ~/.tmux-powerlinerc
-ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
-mkdir -p ~/.config/common-lisp/
-mkdir -p ~/dev/cl
+
+# lisp
+if [[ ! -d ~/.config/common-lisp ]]; then
+    mkdir -p ~/.config/common-lisp
+fi
+if [[ ! -d ~/dev/cl ]]; then
+    mkdir -p ~/dev/cl
+fi
 ln -sf ~/dotfiles/.config/common-lisp/source-registry.conf ~/.config/common-lisp/source-registry.conf
 ln -sf ~/dotfiles/.sbclrc ~/.sbclrc
-mkdir ~/.emacs.d/
+
+# emacs
+if [[ ! -d ~/.emacs.d ]]; then
+    mkdir ~/.emacs.d/
+fi
 ln -sf ~/dotfiles/.emacs.d/init.el ~/.emacs.d/init.el
+
+echo "Finished"
+
--- homebrew_setup.sh
+++ homebrew_setup.sh
@@ -7,3 +7,7 @@ brew install rcmdnk/file/brew-file
 
 brew file set_repo yuya373/brewfile
 brew file install
+cp -f /usr/local/Cellar/ricty/3.2.3/share/fonts/Ricty*.ttf ~/Library/Fonts/
+fc-cache -vf
+
+echo "Finished"
