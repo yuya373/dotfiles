428db345b225ab63a3a8162bf1cc3d239501e735
Yuya Minami <yuya373@me.com>
2016-01-25 00:55:46 +0900

    update lein

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,5 +1,5 @@
 {:user {:plugins [[cider/cider-nrepl "0.10.0-SNAPSHOT"]]
-        :dependencies [[org.clojure/tools.nrepl "0.2.11"]
+        :dependencies [[org.clojure/tools.nrepl "0.2.12"]
                        [acyclic/squiggly-clojure "0.1.3-SNAPSHOT"]]}}
 
 
