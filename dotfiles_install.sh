0f6d51241a086857d5096e83f1d210efd7bc8d2b
Yuya Minami <yuya373@me.com>
2016-05-22 11:18:51 +0900

    update install script

--- dotfiles_install.sh
+++ dotfiles_install.sh
@@ -13,23 +13,15 @@ ln -sf ~/dotfiles/.xvimrc ~/.xvimrc
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/.zshenv ~/.zshenv
 
-# prezto
-if [[ ! -d ~/.zprezto ]]; then
-    ln -sf ~/dotfiles/.zprezto ~/.zprezto
-fi
-ln -sf ~/dotfiles/.zprezto/runcoms/zlogin ~/.zlogin
-ln -sf ~/dotfiles/.zprezto/runcoms/zlogout ~/.zlogout
-ln -sf ~/dotfiles/.zpreztorc ~/.zpreztorc
-ln -sf ~/dotfiles/.zprofile ~/.zprofile
-
 # tmux
-
 if [[ ! -d ~/.tmux/plugins/tpm ]]; then
     git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 fi
+
 if [[ ! -d ~/tmux-powerline ]]; then
     git clone https://github.com/erikw/tmux-powerline.git ~/tmux-powerline
 fi
+
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/.tmux-powerlinerc.default ~/.tmux-powerlinerc
 
@@ -37,20 +29,22 @@ ln -sf ~/dotfiles/.tmux-powerlinerc.default ~/.tmux-powerlinerc
 if [[ ! -d ~/.config/common-lisp ]]; then
     mkdir -p ~/.config/common-lisp
 fi
+
 if [[ ! -d ~/dev/cl ]]; then
     mkdir -p ~/dev/cl
 fi
+
 ln -sf ~/dotfiles/.config/common-lisp/source-registry.conf ~/.config/common-lisp/source-registry.conf
 ln -sf ~/dotfiles/.sbclrc ~/.sbclrc
 
 # emacs
-if [[ ! -d ~/.emacs.d ]]; then
-    mkdir ~/.emacs.d/
-fi
-ln -sf ~/dotfiles/.emacs.d/init.el ~/.emacs.d/init.el
+ln -sf ~/dotfiles/.emacs.d ~/.emacs.d
 
 # eslint
 ln -sf ~/dotfiles/.eslintrc  ~/.eslintrc
+
+# sshrc
+ln -sf ~/dotfiles/.sshrc ~/.sshrc
 
 echo "Finished"
 
--- homebrew_setup.sh
+++ homebrew_setup.sh
@@ -1,13 +1,19 @@
 set -e
 
 # home brew setup
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+if ! which brew > /dev/null; then
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
 
-brew install rcmdnk/file/brew-file
-
-brew file set_repo yuya373/brewfile
-brew file install
-cp -f /usr/local/Cellar/ricty/3.2.3/share/fonts/Ricty*.ttf ~/Library/Fonts/
-fc-cache -vf
+if ! which brew-file > /dev/null; then
+    brew install rcmdnk/file/brew-file
+    brew file set_repo yuya373/brewfile
+    brew file install
+    # ricty
+    cp -f /usr/local/Cellar/ricty/3.2.3/share/fonts/Ricty*.ttf ~/Library/Fonts/
+    fc-cache -vf
+    brew link openssl --force
+fi
 
 echo "Finished"
+
