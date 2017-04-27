e7b23c52ff2d66fafe9eed502f6eb89c59fd8c2e
Yuya Minami <yuya373@me.com>
2017-04-27 16:53:41 +0900

    fix fontconfig

--- dotfiles_install.sh
+++ dotfiles_install.sh
@@ -7,13 +7,17 @@ case ${OSTYPE} in
         ln -sf ~/dotfiles/.Xresources  ~/.Xresources
         ln -sf ~/dotfiles/SandS  ~/SandS
         ln -sf ~/dotfiles/.xbindkeysrc  ~/.xbindkeysrc
-        ln -sf ~/dotfiles/71-no-embedded-bitmaps.conf  ~/.config/fontconfig/conf.d/71-no-embedded-bitmaps.conf
         ln -sf ~/dotfiles/70-synaptics.conf  /etc/X11/xorg.conf.d/70-synaptics.conf
         ln -sf ~/dotfiles/10-monitor.conf  /etc/X11/xorg.conf.d/10-monitor.conf
         if [[ ! -d ~/.config/zathura ]]; then
             mkdir -p ~/.config/zathura
         fi
         ln -sf ~/dotfiles/.config/zathura/zathurarc ~/.config/zathura/zathurarc
+        if [[ ! -d ~/.config/fontconfig/conf.d ]]; then
+            mkdir -p ~/.config/fontconfig/conf.d
+        fi
+        ln -sf ~/dotfiles/.config/fontconfig/conf.d/71-no-embedded-bitmaps.conf  ~/.config/fontconfig/conf.d/71-no-embedded-bitmaps.conf
+        ln -sf ~/dotfiles/.config/fontconfig/conf.d/72-use-twitter-color-emoji.conf ~/.config/fontconfig/conf.d/72-use-twitter-color-emoji.conf
         ;;
 esac
 
