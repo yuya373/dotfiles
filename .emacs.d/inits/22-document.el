21a67efd92942a9be094d5df1476d16c7ae650f5
Yuya Minami <yuya373@me.com>
2017-05-24 19:05:02 +0900

    update init.el

--- .emacs.d/inits/22-document.el
+++ .emacs.d/inits/22-document.el
@@ -115,31 +115,34 @@
     (interactive)
     (my-org-insert-structure "q"))
 
-  (evil-define-key 'visual org-mode-map
-    ",em" 'org-gfm-export-as-markdown
-    ",eM" 'org-gfm-export-to-markdown
-    ",ea" 'org-ascii-export-as-ascii
-    ",eA" 'org-ascii-export-to-ascii
-    )
+  (with-eval-after-load "evil"
+    (evil-define-key 'visual org-mode-map
+      ",em" 'org-gfm-export-as-markdown
+      ",eM" 'org-gfm-export-to-markdown
+      ",ea" 'org-ascii-export-as-ascii
+      ",eA" 'org-ascii-export-to-ascii
+      )
 
-  (evil-define-key 'normal org-mode-map
-    ",t" 'org-todo
-    ",p" 'org-set-property
-    ",it" nil
-    ;; ",it" '(lambda () (interactive) (org-insert-todo-heading nil))
-    ",id" 'org-deadline
-    ",il" 'org-insert-link
-    ",ih" 'org-insert-heading
-    ",ip" 'org-priority
-    ",is" 'org-schedule
-    ",its" 'org-insert-template-src
-    ",ith" 'org-insert-template-html
-    ",itq" 'org-insert-template-quote
-    ",em" 'org-gfm-export-as-markdown
-    ",eM" 'org-gfm-export-to-markdown
-    ",ea" 'org-ascii-export-as-ascii
-    ",eA" 'org-ascii-export-to-ascii
-    (kbd "RET") 'org-open-at-point))
+    (evil-define-key 'normal org-mode-map
+      ",t" 'org-todo
+      ",p" 'org-set-property
+      ",it" nil
+      ;; ",it" '(lambda () (interactive) (org-insert-todo-heading nil))
+      ",id" 'org-deadline
+      ",il" 'org-insert-link
+      ",ih" 'org-insert-heading
+      ",ip" 'org-priority
+      ",is" 'org-schedule
+      ",its" 'org-insert-template-src
+      ",ith" 'org-insert-template-html
+      ",itq" 'org-insert-template-quote
+      ",em" 'org-gfm-export-as-markdown
+      ",eM" 'org-gfm-export-to-markdown
+      ",ea" 'org-ascii-export-as-ascii
+      ",eA" 'org-ascii-export-to-ascii
+      (kbd "RET") 'org-open-at-point)
+    )
+  )
 
 (use-package org-agenda
   :commands (org-agenda)
@@ -148,70 +151,72 @@
   (setq org-agenda-restore-windows-after-quit t)
   :config
   ;; https://gist.github.com/amirrajan/301e74dc844a4c9ffc3830dc4268f177
-  (evil-set-initial-state 'org-agenda-mode 'normal)
-  (evil-define-key 'normal org-agenda-mode-map
-    (kbd "<RET>") 'org-agenda-switch-to
-    (kbd "\t") 'org-agenda-goto
+  (with-eval-after-load "evil"
+    (evil-set-initial-state 'org-agenda-mode 'normal)
+    (evil-define-key 'normal org-agenda-mode-map
+      (kbd "<RET>") 'org-agenda-switch-to
+      (kbd "\t") 'org-agenda-goto
 
-    "q" 'org-agenda-quit
-    "r" 'org-agenda-redo
-    "S" 'org-save-all-org-buffers
-    "gj" 'org-agenda-goto-date
-    "gJ" 'org-agenda-clock-goto
-    "gm" 'org-agenda-bulk-mark
-    "go" 'org-agenda-open-link
-    "s" 'org-agenda-schedule
-    "+" 'org-agenda-priority-up
-    "," 'org-agenda-priority
-    "-" 'org-agenda-priority-down
-    "y" 'org-agenda-todo-yesterday
-    "n" 'org-agenda-add-note
-    "t" 'org-agenda-todo
-    ":" 'org-agenda-set-tags
-    ";" 'org-timer-set-timer
-    "I" 'helm-org-task-file-headings
-    "i" 'org-agenda-clock-in-avy
-    "O" 'org-agenda-clock-out-avy
-    "u" 'org-agenda-bulk-unmark
-    "x" 'org-agenda-exit
-    "j"  'org-agenda-next-line
-    "k"  'org-agenda-previous-line
-    "vt" 'org-agenda-toggle-time-grid
-    "va" 'org-agenda-archives-mode
-    "vw" 'org-agenda-week-view
-    "vl" 'org-agenda-log-mode
-    "vd" 'org-agenda-day-view
-    "vc" 'org-agenda-show-clocking-issues
-    "g/" 'org-agenda-filter-by-tag
-    "o" 'delete-other-windows
-    "gh" 'org-agenda-holiday
-    "gv" 'org-agenda-view-mode-dispatch
-    "f" 'org-agenda-later
-    "b" 'org-agenda-earlier
-    "c" 'helm-org-capture-templates
-    "e" 'org-agenda-set-effort
-    "n" nil  ; evil-search-next
-    "{" 'org-agenda-manipulate-query-add-re
-    "}" 'org-agenda-manipulate-query-subtract-re
-    "A" 'org-agenda-toggle-archive-tag
-    "." 'org-agenda-goto-today
-    "0" 'evil-digit-argument-or-evil-beginning-of-line
-    "<" 'org-agenda-filter-by-category
-    ">" 'org-agenda-date-prompt
-    "F" 'org-agenda-follow-mode
-    "D" 'org-agenda-deadline
-    "H" 'org-agenda-holidays
-    "J" 'org-agenda-next-date-line
-    "K" 'org-agenda-previous-date-line
-    "L" 'org-agenda-recenter
-    "P" 'org-agenda-show-priority
-    "R" 'org-agenda-clockreport-mode
-    "Z" 'org-agenda-sunrise-sunset
-    "T" 'org-agenda-show-tags
-    "X" 'org-agenda-clock-cancel
-    "[" 'org-agenda-manipulate-query-add
-    "g\\" 'org-agenda-filter-by-tag-refine
-    "]" 'org-agenda-manipulate-query-subtract)
+      "q" 'org-agenda-quit
+      "r" 'org-agenda-redo
+      "S" 'org-save-all-org-buffers
+      "gj" 'org-agenda-goto-date
+      "gJ" 'org-agenda-clock-goto
+      "gm" 'org-agenda-bulk-mark
+      "go" 'org-agenda-open-link
+      "s" 'org-agenda-schedule
+      "+" 'org-agenda-priority-up
+      "," 'org-agenda-priority
+      "-" 'org-agenda-priority-down
+      "y" 'org-agenda-todo-yesterday
+      "n" 'org-agenda-add-note
+      "t" 'org-agenda-todo
+      ":" 'org-agenda-set-tags
+      ";" 'org-timer-set-timer
+      "I" 'helm-org-task-file-headings
+      "i" 'org-agenda-clock-in-avy
+      "O" 'org-agenda-clock-out-avy
+      "u" 'org-agenda-bulk-unmark
+      "x" 'org-agenda-exit
+      "j"  'org-agenda-next-line
+      "k"  'org-agenda-previous-line
+      "vt" 'org-agenda-toggle-time-grid
+      "va" 'org-agenda-archives-mode
+      "vw" 'org-agenda-week-view
+      "vl" 'org-agenda-log-mode
+      "vd" 'org-agenda-day-view
+      "vc" 'org-agenda-show-clocking-issues
+      "g/" 'org-agenda-filter-by-tag
+      "o" 'delete-other-windows
+      "gh" 'org-agenda-holiday
+      "gv" 'org-agenda-view-mode-dispatch
+      "f" 'org-agenda-later
+      "b" 'org-agenda-earlier
+      "c" 'helm-org-capture-templates
+      "e" 'org-agenda-set-effort
+      "n" nil  ; evil-search-next
+      "{" 'org-agenda-manipulate-query-add-re
+      "}" 'org-agenda-manipulate-query-subtract-re
+      "A" 'org-agenda-toggle-archive-tag
+      "." 'org-agenda-goto-today
+      "0" 'evil-digit-argument-or-evil-beginning-of-line
+      "<" 'org-agenda-filter-by-category
+      ">" 'org-agenda-date-prompt
+      "F" 'org-agenda-follow-mode
+      "D" 'org-agenda-deadline
+      "H" 'org-agenda-holidays
+      "J" 'org-agenda-next-date-line
+      "K" 'org-agenda-previous-date-line
+      "L" 'org-agenda-recenter
+      "P" 'org-agenda-show-priority
+      "R" 'org-agenda-clockreport-mode
+      "Z" 'org-agenda-sunrise-sunset
+      "T" 'org-agenda-show-tags
+      "X" 'org-agenda-clock-cancel
+      "[" 'org-agenda-manipulate-query-add
+      "g\\" 'org-agenda-filter-by-tag-refine
+      "]" 'org-agenda-manipulate-query-subtract)
+    )
   )
 
 (el-get-bundle open-junk-file)
