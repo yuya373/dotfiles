73b1c6231bb0a3cab96c9f128452957a7eef05ec
Yuya Minami <yuya373@me.com>
2018-02-08 21:37:29 +0900

    fix homebrew setup

--- homebrew_setup.sh
+++ homebrew_setup.sh
@@ -6,15 +6,21 @@ if ! which brew > /dev/null; then
     ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
-if ! which brew-file > /dev/null; then
-    brew install rcmdnk/file/brew-file
-    brew file set_repo yuya373/brewfile
-    brew file install
-    # ricty
-    cp -f /usr/local/Cellar/ricty/3.2.3/share/fonts/Ricty*.ttf ~/Library/Fonts/
-    fc-cache -vf
-    brew link openssl --force
-fi
+# if ! which brew-file > /dev/null; then
+#     brew install rcmdnk/file/brew-file
+#     brew file set_repo yuya373/brewfile
+#     brew file install
+#     # ricty
+#     cp -f /usr/local/Cellar/ricty/3.2.3/share/fonts/Ricty*.ttf ~/Library/Fonts/
+#     fc-cache -vf
+#     brew link openssl --force
+# fi
+
+brew install git hub tmux vim sshrc coreutils zsh nodebrew \
+     the_silver_searcher direnv global ctags \
+     sanemat/font/ricty \
+     reattach-to-user-namespace
+
 
 echo "Finished"
 
