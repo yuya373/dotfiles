983a75d7271e42c7bebb7d0a4eeb7dc2b2001644
Yuya Minami <yuya373@me.com>
2018-09-18 12:52:15 +0900

    update yaskkserv

--- start_yaskkserv.sh
+++ start_yaskkserv.sh
@@ -1,20 +1,6 @@
 #!/bin/bash
 
-/usr/local/sbin/yaskkserv \
-    --max-connection=32 \
+/usr/bin/yaskkserv_hairy \
     --google-japanese-input-timeout=1.0 \
-    --google-japanese-input=notfound-input-suggest \
-    --google-suggest \
-    ~/Dropbox/skk/dict/SKK-JISYO.*.yaskkserv
-# ~/Dropbox/skk/dict/SKK-JISYO.L.yaskkserv \
-    # ~/Dropbox/skk/dict/SKK-JISYO.fullname.yaskkserv \
-    # ~/Dropbox/skk/dict/SKK-JISYO.geo.yaskkserv \
-    # ~/Dropbox/skk/dict/SKK-JISYO.itaiji.yaskkserv \
-    # ~/Dropbox/skk/dict/SKK-JISYO.jinmei.yaskkserv \
-    # ~/Dropbox/skk/dict/SKK-JISYO.law.yaskkserv \
-    # ~/Dropbox/skk/dict/SKK-JISYO.mazegaki.yaskkserv \
-    # ~/Dropbox/skk/dict/SKK-JISYO.office.zipcode.yaskkserv \
-    # ~/Dropbox/skk/dict/SKK-JISYO.propernoun.yaskkserv \
-    # ~/Dropbox/skk/dict/SKK-JISYO.station.yaskkserv \
-    # ~/Dropbox/skk/dict/SKK-JISYO.zipcode.yaskkserv \
-
+    --google-japanese-input=notfound \
+    ~/Dropbox/skk/dict/SKK-JISYO-MERGED.yaskkserv
--- yaskkserv_install.sh
+++ yaskkserv_install.sh
@@ -2,7 +2,9 @@
 
 # git clone git@github.com:wachikun/yaskkserv.git
 # cd yaskkserv
-make distclean
-./configure --enable-google-japanese-input --enable-google-suggest --enable-systemd
-make
-sudo make install_hairy
+# make distclean
+# ./configure --enable-google-japanese-input --enable-systemd
+# make
+# sudo make install_hairy
+
+echo "yay -S  yaskkserv"
