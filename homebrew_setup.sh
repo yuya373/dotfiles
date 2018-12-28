4f733bde27b307d9aea3d2a9ad013efc4e04e3a7
yuya minami <yuya373@me.com>
2018-12-28 19:49:50 +0900

    update homebrew

--- homebrew_setup.sh
+++ homebrew_setup.sh
@@ -6,16 +6,6 @@ if ! which brew > /dev/null; then
     ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
-# if ! which brew-file > /dev/null; then
-#     brew install rcmdnk/file/brew-file
-#     brew file set_repo yuya373/brewfile
-#     brew file install
-#     # ricty
-#     cp -f /usr/local/Cellar/ricty/3.2.3/share/fonts/Ricty*.ttf ~/Library/Fonts/
-#     fc-cache -vf
-#     brew link openssl --force
-# fi
-
 brew cask install dash alfred bettertouchtool \
      google-chrome karabiner-elements \
      bitbar android-platform-tools android-sdk
@@ -26,12 +16,16 @@ brew install cairo --with-x11
 brew install git hub tmux vim sshrc coreutils zsh \
      cmigemo ispell \
      the_silver_searcher direnv global ctags \
-     sanemat/font/ricty reattach-to-user-namespace \
+     reattach-to-user-namespace \
      yarn hugo carthage \
-     mono
+     mono go
 
 brew install texinfo
 brew link --force texinfo
+
+brew install ricty --with-patch-in-place
+cp -f /usr/local/opt/ricty/share/fonts/Ricty*.ttf ~/Library/Fonts/
+fc-cache -vf
 
 
 echo "Finished"
