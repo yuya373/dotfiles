a168049909901c282458475a9b325033113731df
南優也 <yuya373@me.com>
2018-12-29 14:09:04 +0900

    update homebrew setup

--- homebrew_setup.sh
+++ homebrew_setup.sh
@@ -8,7 +8,8 @@ fi
 
 brew cask install dash alfred bettertouchtool \
      google-chrome karabiner-elements \
-     bitbar android-platform-tools android-sdk
+     bitbar android-platform-tools android-sdk \
+     dropbox aquaskk
 
 brew cask install xquartz
 brew install cairo --with-x11
@@ -18,11 +19,20 @@ brew install git hub tmux vim sshrc coreutils zsh \
      the_silver_searcher direnv global ctags \
      reattach-to-user-namespace \
      yarn hugo carthage \
-     mono go
+     mono go dep
+
+# build Emacs
+# brew install autoconf
+# brew install --build-from-source gnutls
+
+brew install emacs --with-cocoa --with-ctags --with-imagemagick@6 --with-librsvg --with-dbus --HEAD
+# pdf-tools
+brew install autoconf automake poppler
 
 brew install texinfo
 brew link --force texinfo
 
+brew tap sanemat/font
 brew install ricty --with-patch-in-place
 cp -f /usr/local/opt/ricty/share/fonts/Ricty*.ttf ~/Library/Fonts/
 fc-cache -vf
