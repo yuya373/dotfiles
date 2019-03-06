173f7073d5b2b9af1e6a990619c338911c69970f
南優也 <yuya373@me.com>
2019-03-07 00:36:57 +0900

    update init.el

---
+++ .emacs.d/inits/45-terraform.el
@@ -0,0 +1,34 @@
+;;; 45-terraform.el ---                              -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2019  南優也
+
+;; Author: 南優也 <yuya373@minamiyuuyanoMacBook-Pro.local>
+;; Keywords:
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;
+
+;;; Code:
+
+
+(el-get-bundle terraform-mode)
+(use-package terraform-mode
+  :mode (("\\.tf\\(vars\\)?\\'" . terraform-mode)))
+
+
+(provide '45-terraform)
+;;; 45-terraform.el ends here
