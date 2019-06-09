d337cd774ba91b441a79f75669c5f1aa1f485792
Yuya Minami <yuya373@me.com>
2019-06-09 16:18:52 +0900

    Update yarn

--- yarn_setup.sh
+++ yarn_setup.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-yarn global add marked expo-cli
+yarn global add \
+     marked \
+     expo-cli \
+     vscode-html-languageserver-bin \
+     typescript-language-server
+     typescript \
