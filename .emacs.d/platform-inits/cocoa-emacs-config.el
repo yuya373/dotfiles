4ca3e3b8ef5848eb2e11f0df737938f5e5548d85
Yuya Minami <yuya373@me.com>
2024-12-13 11:25:53 +0900

    Update init.el

--- .emacs.d/platform-inits/cocoa-emacs-config.el
+++ .emacs.d/platform-inits/cocoa-emacs-config.el
@@ -62,7 +62,7 @@
   (setq migemo-command "cmigemo")
   (setq migemo-options '("-q" "--emacs"))
   ;; Set your installed path
-  (setq migemo-dictionary "/usr/local/share/migemo/utf-8/migemo-dict")
+  (setq migemo-dictionary "/opt/homebrew/share/migemo/utf-8/migemo-dict")
 
   (setq migemo-user-dictionary nil)
   (setq migemo-regex-dictionary nil)
